05e99eba447a96ac31c97217628917132dbdfb15
KalebHawkins <KalebHawkins@outlook.com>
2025-04-24 21:00:53 -0500

    update lambdas to see to test cdk lambda updates

--- services/notifyAdmins/handler.py
+++ services/notifyAdmins/handler.py
@@ -1,3 +1,4 @@
 def lambda_handler(event, context):
     print("Notifying admins...")
+    print("NOTIFYING INTENSIFIES")
     return { "message": "Admins notified!" }
--- services/processUsers/handler.py
+++ services/processUsers/handler.py
@@ -1,3 +1,4 @@
 def lambda_handler(event, context):
     print("Processing users...")
+    print("PROCESSING INTENSIFIES")
     return { "message": "Processed users!" }
