a8253b71a397dfd06cb9e20f02b17665d7c970d7
KalebHawkins <KalebHawkins@outlook.com>
2025-04-28 07:47:29 -0500

    revert to previous lambda version

--- services/notifyAdmins/handler.py
+++ services/notifyAdmins/handler.py
@@ -1,4 +1,3 @@
 def lambda_handler(event, context):
     print("Notifying admins...")
-    print("NOTIFYING INTENSIFIES")
     return { "message": "Admins notified!" }
--- services/processUsers/handler.py
+++ services/processUsers/handler.py
@@ -1,4 +1,3 @@
 def lambda_handler(event, context):
     print("Processing users...")
-    print("PROCESSING INTENSIFIES")
     return { "message": "Processed users!" }
